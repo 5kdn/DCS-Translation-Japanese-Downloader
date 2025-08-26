9f6a0e59077e57fb2233d7232d0db997cfceda30
5kdn <sakuden542+github@gmail.com>
2025-08-26 18:15:31 +0900

    feat: add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+// biome-ignore lint/correctness/noUnusedVariables: Templateで使用している
+const Footer = defineAsyncComponent(() => import('./components/Footer.vue'));
 </script>
 
 
 <template lang="pug">
 v-container#app-wrapper.pt-10
+  h1.text-h1.text-center DCS Translation Japanese
+  v-divider
+v-container
+  Footer
 </template>
 
 
