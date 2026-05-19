cfa842d88ca0d99304305d4b122a0fa2d3acfc4e
5kdn <sakuden542+github@gmail.com>
2026-05-20 02:45:34 +0900

    chore(master): release 3.0.0

--- src/features/mizTranslation/mizTranslationGeneratorVersion.ts
+++ src/features/mizTranslation/mizTranslationGeneratorVersion.ts
@@ -1,4 +1,4 @@
 /**
  * @summary X-Generator に埋め込むアプリケーション version を表す。
  */
-export const MIZ_TRANSLATION_GENERATOR_VERSION = '2.1.0'; // <!-- x-release-please-version -->
+export const MIZ_TRANSLATION_GENERATOR_VERSION = '3.0.0'; // <!-- x-release-please-version -->
